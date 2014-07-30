ca5c4e664e422912d968d2ab5fec314625137e26
skorlir <skorlir@gmail.com>
2014-07-30 04:41:15 -0500

    remove unused utility fn

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,16 +25,6 @@ function extend(el) {
   return el;
 }
 
-function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16),
-    toString: function() { [r, g, b].join(','); }
-  } : hex;
-}
-
 function nameToDataTag(txt) {
   return txt.trim()
              //alternatively, 
